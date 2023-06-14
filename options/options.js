f9ca185859962533fa02b8be2d4643bcfe2034ae
Crowdbotics <engineering@crowdbotics.com>
2023-06-14 12:44:36 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "orange-mouse-dev-91494",
+  url: "https://orange-mouse-dev-91494.botics.co",
+  api: "https://orange-mouse-dev-91494.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
